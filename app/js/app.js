5c7659f73edcaef274fda7b34dd0284509fefd65
Mike Fisher <michael.fisher@bskyb.com>
2014-08-28 14:42:42 +0100

    Rename app and add firebase

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,14 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('skySquash', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'skySquash.filters',
+  'skySquash.services',
+  'skySquash.directives',
+  'skySquash.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/', {templateUrl: 'partials/index.html', controller: 'IndexCtrl'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
