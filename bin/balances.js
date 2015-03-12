d1dafa7c07f79545114302ab21f1c070194eba3e
Mike Fisher <mike@mbfisher.com>
2015-03-12 09:20:46 +0000

    Add total to balance script

--- bin/balances.js
+++ bin/balances.js
@@ -12,11 +12,16 @@ ref.child('bookings').once('value', function (snapshot) {
         return booking.isComplete();
     });
 
+    var total = 0;
     ref.child('users').once('value', function (snapshot) {
         var balance;
         _.each(snapshot.val(), function (user) {
             balance = userBalance(user, bookings);
+            total += balance;
             console.log(user.displayName, balance.toFixed(2));
         });
+
+        console.log('TOTAL', total.toFixed(2));
+        process.exit(0);
     });
 });
